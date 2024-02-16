6c5c60bc02fdb41b8250d8ef12874753e2f0daff
E-jane15 <janeebaimay@gmail.com>
2024-02-16 21:01:46 +0100

    added code to display results on page

--- Quiz Game/script.js
+++ Quiz Game/script.js
@@ -1,16 +1,21 @@
 const correctAnswers = ['B','A','B','A'];
 const form = document.querySelector(".quiz-form");
-
+const result = document.querySelector(".result");
 form.addEventListener("submit", e => {
     e.preventDefault();
 
     let score = 0;
     const userAnswers =[form.q1.value, form.q2.value, form.q3.value, form.q4.value];
 
-    userAnswers.forEach((answer,index) =>{
+    //cheks answers
+
+    userAnswers.forEach((answer,index) =>{ 
       if(answer === correctAnswers[index]){
         score += 25; 
       } 
     });
-    console.log(score);  
+    
+    //shows result on quiz page
+   result.querySelector("span"). textContent = `${score}%`;
+   result.classList.remove("d-none");
 });
