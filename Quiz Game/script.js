b10fff9f348502e2d41603d8f59dc60a525f21ac
E-jane15 <janeebaimay@gmail.com>
2024-02-16 21:30:35 +0100

    added code to animate score

--- Quiz Game/script.js
+++ Quiz Game/script.js
@@ -1,7 +1,8 @@
 const correctAnswers = ['B','A','B','A'];
 const form = document.querySelector(".quiz-form");
 const result = document.querySelector(".result");
-form.addEventListener("submit", e => {
+
+ form.addEventListener("submit", e => {
     e.preventDefault();
 
     let score = 0;
@@ -16,6 +17,18 @@ form.addEventListener("submit", e => {
     });
     
     //shows result on quiz page
+    scrollTo(0,0);
    result.querySelector("span"). textContent = `${score}%`;
    result.classList.remove("d-none");
+
+    //animating score
+   let output = 0;
+   const timer = setInterval(() => {
+    result.querySelector("span").textContent = `${output}%`;
+    if(output === score){
+        clearInterval(timer);
+    }else {
+        output++;
+    }
+   }, 10);
 });
