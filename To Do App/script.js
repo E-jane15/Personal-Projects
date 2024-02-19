5f9a7f1624ecbb56837901be50a87444b6ac316d
E-jane15 <janeebaimay@gmail.com>
2024-02-19 23:40:21 +0100

    wrapped input in form tag and made appropriate references in js file

--- To Do App/script.js
+++ To Do App/script.js
@@ -1,5 +1,14 @@
 const toDo = [''];
-const search = document.querySelector('.search');
+const search = document.querySelector('.search-form');
 const cancel = document.querySelectorAll('img');
-const add = document.querySelector('.new');
+const add = document.querySelector('.newform');
+const task = document.querySelector('.task');
 
+
+add.addEventListener(e => {
+    let str = '';
+    e.target =
+    toDo = toDo.add('str');
+    task = toDo;
+
+})
