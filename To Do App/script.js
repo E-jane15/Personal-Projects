63387743e0ead4227f4fa7788a85be36179c3e62
E-jane15 <janeebaimay@gmail.com>
2024-02-20 22:30:07 +0100

    added function to new task input field to ensure new todos are added to list when submitted

--- To Do App/script.js
+++ To Do App/script.js
@@ -1,14 +1,25 @@
-const toDo = [''];
 const search = document.querySelector('.search-form');
 const cancel = document.querySelectorAll('img');
-const add = document.querySelector('.newform');
-const task = document.querySelector('.task');
+const addForm  = document.querySelector('.newform');
+const list = document.querySelector('.lists');
+
+addForm.addEventListener('submit', e => {
+ 
+    e.preventDefault();
+   const todo = addForm.add.value.trim();
+    generateTemplate(todo);
+});
+
+const generateTemplate = () => {
+   const todo = addForm.add.value.trim();
+    const html = `
+    <div class="task">
+    <li>${todo}</li>
+    <img src="/To Do App/delete icon.png" >
+    </div>
+   `;
+ 
+    list.innerHTML += html; 
 
 
-add.addEventListener(e => {
-    let str = '';
-    e.target =
-    toDo = toDo.add('str');
-    task = toDo;
-
-})
+}
