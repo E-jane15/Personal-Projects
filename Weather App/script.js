46dc20f882cc48eacf471f92b3158cf0ca13006d
E-jane15 <janeebaimay@gmail.com>
2024-02-27 00:15:56 +0100

    updated api link

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,3 +1,10 @@
-const apiKey = "863242cfb2b1d357e6093d9a4df19a4b";
-const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+const apiKey = "f7c6ba02aa2b16b356ea8893ec8dc384";
+const apiUrl ="https://api.openweathermap.org/data/2.5/weather?&units=metric";
 
+async function checkWeather(){
+    const response =await fetch(apiUrl + `@appid=$(apiKey)`);
+    let data =await response.json();
+
+    console.log(data);
+}
+checkWeather();
