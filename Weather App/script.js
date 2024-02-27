d98e5b98c318e324a889001125620e6e7a2558e8
E-jane15 <janeebaimay@gmail.com>
2024-02-27 16:25:21 +0100

    added code to change weather icon with respect to the temperature

--- Weather App/script.js
+++ Weather App/script.js
@@ -7,7 +7,7 @@ const humidity = document.querySelector('.humidity')
 const wind = document.querySelector('.wind');
 const searchBar = document.querySelector('.search input');
 const searchBtn = document.querySelector('.search button');
-
+const weatherIcon = document.querySelector('#weather');
 
 async function checkWeather(town){
     const response = await fetch(apiUrl  + town + `&appid=${apiKey}`);
@@ -19,7 +19,21 @@ async function checkWeather(town){
   temp.innerHTML=data.main.temp + "°c";
   humidity.innerHTML=data.main.humidity + "%";
   wind.innerHTML=data.wind.speed + "km/h";
+  
 
+  if(data.weather[0].main == 'Clouds'){
+     weatherIcon.src='images/clouds.png';
+  } else if(data.weather[0].main == 'Clear'){
+    weatherIcon.src='images/clear.png';
+  } else if(data.weather[0].main == 'Drizzle'){
+    weatherIcon.src='images/drizzle.png';
+  } else if(data.weather[0].main == 'Mist'){
+    weatherIcon.src='images/mist.png';
+  }else if(data.weather[0].main == 'Rain'){
+    weatherIcon.src='images/rain.png';
+  } else if(data.weather[0].main == 'Snow'){
+    weatherIcon.src='images/snow.png';
+  } 
 
 }
 
