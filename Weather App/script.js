670dfa79639888513ec72001ed05fc82d6fdbf5b
E-jane15 <janeebaimay@gmail.com>
2024-02-27 01:05:28 +0100

    showed response of city, humidiy, temperature and windspeed on webpage

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,10 +1,25 @@
+
 const apiKey = "f7c6ba02aa2b16b356ea8893ec8dc384";
-const apiUrl ="https://api.openweathermap.org/data/2.5/weather?&units=metric";
+const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+const city = document.querySelector('.city');
+const temp = document.querySelector('.temperature');
+const humidity = document.querySelector('.humidity')
+const wind = document.querySelector('.wind');
+
+
+
 
 async function checkWeather(){
-    const response =await fetch(apiUrl + `@appid=$(apiKey)`);
-    let data =await response.json();
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    let data = await response.json();
 
     console.log(data);
+    
+  city.innerHTML = data.name;
+  temp.innerHTML=data.main.temp + "°c";
+  humidity.innerHTML=data.main.humidity + "%";
+  wind.innerHTML=data.wind.speed + "km/h";
+
+
 }
 checkWeather();
