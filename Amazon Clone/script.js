17659828de2dc87c3bb68aace653ae88f3a85105
E-jane15 <janeebaimay@gmail.com>
2024-03-10 18:13:05 +0100

    added transparent layer at bottom of the images and added js code for arrow buttons

---
+++ Amazon Clone/script.js
@@ -0,0 +1,36 @@
+const next = document.querySelector('.next');
+const previous = document.querySelector('.previous');
+const images = document.querySelectorAll('.main-background ul img');
+
+
+let n = 0;
+
+function slide (){
+ for (let i = 0; i < images.length; i++) {
+    images[i].style.display = 'none';
+ }
+ images[n].style.display = 'block';
+};
+
+previous.addEventListener('click', (e) =>{
+     e.preventDefault;
+     if(n > 0){
+       n = n-1;
+     }else {
+        n = images.length-1;
+     }
+     slide();
+});
+
+next.addEventListener('click', (e) => {
+    e.preventDefault;
+    if(n < images.length-1){
+        n = n+1;
+    }else {
+        n = 0;
+    }
+
+     
+     slide();
+
+});
