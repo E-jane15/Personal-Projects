b9ce914ed2a48ddcb37a240c93f6abd6fe27a97c
E-jane15 <janeebaimay@gmail.com>
2024-03-24 13:28:53 +0100

    changed variable name and id for name  div and added event listener to form

--- Login and Registration Form/script.js
+++ Login and Registration Form/script.js
@@ -1,15 +1,17 @@
 const signin = document.querySelector('.signin');
 const signup = document.querySelector('.signup');
-const userName = document.getElementById('name');
+const nameSection = document.getElementById('name-section');
 const text = document.querySelector('p');
 const title = document.querySelector('h1');
+const form = document.getElementById('form')
+
 
 signin.addEventListener('click',()=>{
     signin.style.background = '#fc6e10';
     signin.style.color ='#ffff';
     signup.style.background = '#eaeaea';
     signup.style.color ='#555';
-    userName.style.display = 'none';
+    nameSection.style.display = 'none';
     text.style.display ='block';
     title.innerHTML = 'Sign In';
 });
@@ -19,7 +21,11 @@ signup.addEventListener('click',()=>{
     signin.style.color ='#555';
     signup.style.background = '#fc6e10';
     signup.style.color ='#ffff';
-    userName.style.display = 'flex';
+    nameSection.style.display = 'flex';
     text.style.display ='none';
     title.innerHTML = 'Sign Up';
 });
+
+form.addEventListener('submit', (e)=> {
+
+})
